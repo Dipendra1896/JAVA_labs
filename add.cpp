6e32ede69279d4d22855f30d0da6647e42f4056a
Dipendra1896 <dipendrapatel371896@gmail.com>
2023-06-20 03:28:47 +0545

    dsa labs

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Addition{
+    public:
+    int a,b,sum;
+    public:
+    void add(){
+        cout<<"enter two integers:"<<endl;
+        cin>>a>>b;
+        sum=a+b;
+        cout<<"sum is:"<<sum;
+    };
+};
+int main(){
+    Addition a;
+    a.add();
+};
